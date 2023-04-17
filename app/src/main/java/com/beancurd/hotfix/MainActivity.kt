87da4960cf85c8ff671ca44baef945f061757100
z69yuan <z69yuan@163.com>
2023-04-17 22:02:31 +0800

    fix bug

--- app/src/main/java/com/beancurd/hotfix/MainActivity.kt
+++ app/src/main/java/com/beancurd/hotfix/MainActivity.kt
@@ -2,10 +2,21 @@ package com.beancurd.hotfix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tv_onclick).setOnClickListener {
+            showToast();
+        }
+    }
+
+    private fun showToast() {
+        val i = 5/0
+        Toast.makeText(this,"hello world",0).show()
     }
 }
