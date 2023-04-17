c4330bde33617c13107cd7f1bc321bc956db84e6
z69yuan <z69yuan@163.com>
2023-04-18 06:43:23 +0800

    重构一下代码

--- app/src/main/java/com/beancurd/hotfix/MyApp.kt
+++ app/src/main/java/com/beancurd/hotfix/MyApp.kt
@@ -2,9 +2,7 @@ package com.beancurd.hotfix
 
 import android.app.Application
 import android.content.Context
-import com.beancurd.common.utils.LogE
-import dalvik.system.BaseDexClassLoader
-import java.io.File
+import com.beancurd.hotfix.utls.installNewDex
 
 class MyApp : Application() {
 
@@ -14,48 +12,7 @@ class MyApp : Application() {
         super.attachBaseContext(base)
         base?:return
 
-        installNewDex(base)
+        installNewDex(base, classLoader)
     }
 
-    private fun installNewDex(context: Context) {
-
-        LogE(TAG, "*********************************")
-        // 1. 加载插件包
-//        val file = File(base.cacheDir,"appv1.apk")
-        val file = File(context.cacheDir,"classes3.dex")
-        if(!file.exists()){
-            LogE(TAG, "$file does not exist ....")
-            return
-        }
-
-        // String dexPath, File optimizedDirectory, String librarySearchPath, ClassLoader parent
-        val loader = BaseDexClassLoader(file.absolutePath,null,null,null)
-        val newElements = getDexElement(loader)
-        LogE(TAG,"newElements is : ${newElements.size}")
-        val oldElements = getDexElement(classLoader as BaseDexClassLoader)
-        LogE(TAG,"oldElements is : ${oldElements.size}")
-        // 2. 全局替换类
-        val arr = java.lang.reflect.Array.newInstance(
-            oldElements[0]!!.javaClass,
-            newElements.size + oldElements.size
-        ) as Array<Any>
-
-        val len = arr.size
-        val newLen = newElements.size
-        for (i in 0 until len) {
-            arr.set(i, if(i < newLen) {
-                newElements[i]!!
-            } else {
-                oldElements[i-newLen]!!
-            })
-        }
-
-//        LogE(TAG,"setDexElement isSuccess : $arr")
-        val isSuccess = setDexElement(classLoader as BaseDexClassLoader,arr)
-        LogE(TAG,"setDexElement isSuccess : $isSuccess")
-
-        val recentElement = getDexElement(classLoader as BaseDexClassLoader)
-        LogE(TAG,"recentElement is : ${recentElement.size}")
-
-    }
 }
